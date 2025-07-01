c0e093095981a69cd9eae79b6c39dbb9fc5cc559
shravani556 <38123454+shravani556@users.noreply.github.com>
2025-07-01 18:29:31 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "your-s3-bucket-name"
-    key    = "envs/${var.environment}/terraform.tfstate"
+    bucket = "java-bucket7013"
+    key    = "envs/qa/terraform.tfstate"
     region = "us-east-1"
   }
 }
